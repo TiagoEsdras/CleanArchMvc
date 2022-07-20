2fcf8e904eba6fe09fb9fae4abc99b05c8d67f16
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-07-20 18:32:49 -0300

    Enriquecendo modelo Category

--- CleanArchMvc/CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc/CleanArchMvc.Domain/Entities/Category.cs
@@ -1,11 +1,41 @@
-﻿using System.Collections.Generic;
+﻿using CleanArchMvc.Domain.Validation;
+using System.Collections.Generic;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public class Category
+    public sealed class Category
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
         public ICollection<Product> Products { get; set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0,
+                "Invalid Id value");
+            ValidateDomain(name);
+            Id = id;
+        }
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name. Name is required.");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name. Minimum 3 characters.");
+
+            Name = name;
+        }
     }
 }
