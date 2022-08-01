7299154d9ad17be08a194b8579f106451709bdfc
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-08-01 20:31:40 -0300

    Criando categoryRepository e implementando metodos

---
+++ CleanArchMvc/CleanArchMvc.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,49 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext context;
+
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Category> CreateAsync(Category category)
+        {
+            context.Add(category);
+            await context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task DeleteAsync(Category category)
+        {
+            context.Remove(category);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<Category> GetByIdAsync(int? id)
+        {
+            return await context.Categories.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Category>> GetCategoriesAsync()
+        {
+            return await context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> UpdateAsync(Category category)
+        {
+            context.Update(category);
+            await context.SaveChangesAsync();
+            return category;
+        }
+    }
+}
