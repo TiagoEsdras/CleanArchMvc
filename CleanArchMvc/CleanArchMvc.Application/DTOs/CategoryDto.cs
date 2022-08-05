564b361413a68d4b4a33f0d85f973e72c8f060c1
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-08-04 22:44:45 -0300

    Criando categoryDto e productDto

---
+++ CleanArchMvc/CleanArchMvc.Application/DTOs/CategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMvc.Application.DTOs
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
---
+++ CleanArchMvc/CleanArchMvc.Application/DTOs/ProductDto.cs
@@ -0,0 +1,38 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CleanArchMvc.Application.DTOs
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [MinLength(5)]
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Stock is required")]
+        [Range(1, 9999)]
+        public decimal Stock { get; set; }
+
+        [MaxLength(250)]
+        public string Image { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
