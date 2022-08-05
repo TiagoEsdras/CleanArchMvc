705e337735a88ca63a3adb51708224a4cd06989a
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-08-04 22:45:14 -0300

    Criando configuração de automapper entre dominio e dtos

---
+++ CleanArchMvc/CleanArchMvc.Application/Mappings/DomainToDtoMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Application.Mappings
+{
+    public class DomainToDtoMappingProfile : Profile
+    {
+        public DomainToDtoMappingProfile()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Product, ProductDto>().ReverseMap();
+        }
+    }
+}
