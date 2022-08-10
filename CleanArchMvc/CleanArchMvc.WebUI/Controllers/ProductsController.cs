c2cba01ca28e98d4173220b52531fc19a630cf14
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-08-09 21:12:39 -0300

    criando products controller

---
+++ CleanArchMvc/CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService productService;
+
+        public ProductsController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var products = await productService.GetProducts();
+            return View(products);
+        }
+    }
+}
