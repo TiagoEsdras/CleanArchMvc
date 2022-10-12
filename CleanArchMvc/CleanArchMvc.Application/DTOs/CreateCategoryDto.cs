6f1376b7f74edb01b9bb184f0f7b3fad7fd2376c
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-10-11 22:06:45 -0300

    Criando clase CreateCategoryDto

---
+++ CleanArchMvc/CleanArchMvc.Application/DTOs/CreateCategoryDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMvc.Application.DTOs
+{
+    public class CreateCategoryDto
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
