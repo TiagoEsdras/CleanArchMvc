4e466231000ff5f633d6fb13b9be1155bdf37b12
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-10-11 21:22:11 -0300

    implementando get categories no projeto API

---
+++ CleanArchMvc/CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -0,0 +1,30 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDto>>> Get()
+        {
+            var categories = await categoryService.GetCategories();
+
+            if (categories is null) return NotFound("Categories not found");
+
+            return Ok(categories);
+        }
+    }
+}
