a3fc75499b840d8fc873fc3189c148b98867755d
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-10-12 13:56:15 -0300

    implementando AuthenticateService

---
+++ CleanArchMvc/CleanArchMvc.Infra.Data/Identity/AuthenticateService.cs
@@ -0,0 +1,46 @@
+﻿using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.Identity
+{
+    public class AuthenticateService : IAuthenticate
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public AuthenticateService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public async Task<bool> Authenticate(string email, string password)
+        {
+            var result = await signInManager.PasswordSignInAsync(email, password, false, lockoutOnFailure: false);
+
+            return result.Succeeded;
+        }
+
+        public async Task<bool> RegisterUser(string email, string password)
+        {
+            var applicationUser = new ApplicationUser()
+            {
+                Email = email,
+                UserName = email
+            };
+
+            var result = await userManager.CreateAsync(applicationUser, password);
+
+            if (result.Succeeded)
+                await signInManager.SignInAsync(applicationUser, isPersistent: false);
+
+            return result.Succeeded;
+        }
+
+        public async Task Logout()
+        {
+            await signInManager.SignOutAsync();
+        }
+    }
+}
